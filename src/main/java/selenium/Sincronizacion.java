2ffaf1946a2c193b7a98a3c9e9c500e15501a07b
lalmenares <liudmila.sanchez@gmail.com>
2021-01-24 21:57:57 -0800

    adicionando clase de sincronizacion

--- src/main/java/selenium/Sincronizacion.java
+++ src/main/java/selenium/Sincronizacion.java
@@ -12,9 +12,10 @@ public class Sincronizacion {
 
 	public static void main(String[] args) throws InterruptedException {
 	         DataManager dato = new DataManager();
-		 String usuario = dato.getDato("usuario", 0);
+		 String usuario_valor = dato.getDato("usuario", 0);
+		 String usuario_id = "//*[@id='user-name']";
 		 String contrasenna =dato.getDato("pass", 0);		
-	         //Obtenemos el dato que se encuentra en la primera posicion de la columna nombre.
+	         //Obtenemos el dato que se encuentra en la primera posicion de la columna nombre..........
 	    
         //Precondiciones
         System.setProperty("webdriver.chrome.driver", "./src/main/java/drivers/chromedriver.exe");
@@ -24,7 +25,7 @@ public class Sincronizacion {
         //Vamos a utilizar este recurso donde vamos a poder utilizar una pagina adicional
 
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//*[@id='user-name']")).sendKeys(usuario);
+        driver.findElement(By.xpath(usuario_id)).sendKeys(usuario_valor);
         driver.findElement(By.xpath("//*[@id='password']")).sendKeys(contrasenna);
         driver.findElement(By.xpath("//*[@id='login-button']")).click();
          //Maximizando la pantalla
